3f2e6062e45a6593753051fe995a3a30c5c81f6b
elizabet-bagdasaryan <elizabet.bagdasaryan1@gmail.com>
2023-05-02 12:37:20 +0400

    "feat: add filtering  and deleting functionality"

--- src/ToDos.jsx
+++ src/ToDos.jsx
@@ -1,3 +1,60 @@
+// import { useState } from "react";
+
+// import CloseIcon from "@mui/icons-material/Close";
+// import ToDoItem from "./ToDoItem.jsx";
+
+// import "./ToDos.css";
+
+// function ToDos({ todos, toggleTodo, deleteTodo }) {
+//   const count = todos.length;
+//   const itemText = count === 1 ? "item left" : "items left";
+//   const handleClearClick = () => {
+//     todos.forEach((todo) => deleteTodo(todo.id));
+//   };
+//   return (
+//     <div className="list">
+//       <div className="items">
+//         {todos.map((todo) => {
+//           return (
+//             <ToDoItem
+//               {...todo}
+//               key={todo.id}
+//               toggleTodo={toggleTodo}
+//               deleteTodo={deleteTodo}
+//             />
+//           );
+//         })}
+
+//         <div id="items-clear">
+//           <p>
+//             {" "}
+//             {count} {itemText} left
+//           </p>
+//           <p onClick={handleClearClick}>Clear</p>
+//         </div>
+//         <div className="desk-last-section">
+//           <p>
+//             {count} {itemText} left
+//           </p>
+//           <div>
+//             <p>All</p>
+//             <p>Active</p>
+//             <p>Completed</p>
+//           </div>
+//           <p onClick={handleClearClick}>Clear</p>
+//         </div>
+//       </div>
+//       <div className="filters">
+//         <p>All</p>
+//         <p>Active</p>
+//         <p>Completed</p>
+//       </div>
+//     </div>
+//   );
+// }
+
+// export default ToDos;
+
 import { useState } from "react";
 
 import CloseIcon from "@mui/icons-material/Close";
@@ -6,25 +63,29 @@ import ToDoItem from "./ToDoItem.jsx";
 import "./ToDos.css";
 
 function ToDos({ todos, toggleTodo, deleteTodo }) {
-  const count = todos.length;
-  console.log(count);
-  const [block, setBlock] = useState(false);
+  const [filter, setFilter] = useState("all");
 
-  function handleZero() {
-    if (todos.length === 0) {
-      setBlock(true);
+  const handleClearClick = () => {
+    todos.forEach((todo) => deleteTodo(todo.id));
+  };
+
+  const filteredTodos = todos.filter((todo) => {
+    if (filter === "active") {
+      return !todo.completed;
+    } else if (filter === "completed") {
+      return todo.completed;
+    } else {
+      return true;
     }
-  }
+  });
+
+  const count = filteredTodos.length;
+  const itemText = count === 1 ? "item left" : "items left";
 
   return (
-    <div
-      className="list"
-      style={{
-        display: block ? "none" : "block",
-      }}
-    >
+    <div className="list">
       <div className="items">
-        {todos.map((todo) => {
+        {filteredTodos.map((todo) => {
           return (
             <ToDoItem
               {...todo}
@@ -36,23 +97,43 @@ function ToDos({ todos, toggleTodo, deleteTodo }) {
         })}
 
         <div id="items-clear">
-          <p> {count} items left</p>
-          <p onClick={handleZero}>Clear</p>
+          <p>
+            {" "}
+            {count} {itemText}
+          </p>
+          <p onClick={handleClearClick}>Clear</p>
         </div>
         <div className="desk-last-section">
-          <p>{count} items left</p>
+          <p>
+            {count} {itemText}
+          </p>
           <div>
-            <p>All</p>
-            <p>Active</p>
-            <p>Completed</p>
+            <p onClick={() => setFilter("all")}>All</p>
+            <p onClick={() => setFilter("active")}>Active</p>
+            <p onClick={() => setFilter("completed")}>Completed</p>
           </div>
-          <p>Clear</p>
+          <p onClick={handleClearClick}>Clear</p>
         </div>
       </div>
       <div className="filters">
-        <p>All</p>
-        <p>Active</p>
-        <p>Completed</p>
+        <p
+          className={filter === "all" ? "active" : ""}
+          onClick={() => setFilter("all")}
+        >
+          All
+        </p>
+        <p
+          className={filter === "active" ? "active" : ""}
+          onClick={() => setFilter("active")}
+        >
+          Active
+        </p>
+        <p
+          className={filter === "completed" ? "active" : ""}
+          onClick={() => setFilter("completed")}
+        >
+          Completed
+        </p>
       </div>
     </div>
   );
